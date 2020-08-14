f8385a793bd1fcaca3c1f98bf41e6ddd21f9696d
Joseph Larmarange <joseph@larmarange.net>
2020-08-14 17:37:51 +0200

    restore test for add_reference_rows

--- tests/testthat/test-add_reference_rows.R
+++ tests/testthat/test-add_reference_rows.R
@@ -1,117 +1,62 @@
-test_that("tidy_add_header_rows() works as expected", {
+test_that("tidy_add_reference_rows() works as expected", {
   mod <- glm(
     response ~ stage + grade * trt,
     gtsummary::trial,
     family = binomial,
     contrasts = list(stage = contr.treatment, grade = contr.SAS, trt = contr.sum)
   )
-  res <- mod %>% tidy_and_attach() %>% tidy_add_header_rows()
-  expect_equivalent(
-    res$label,
-    c("(Intercept)", "T Stage", "T2", "T3", "T4", "Grade", "I", "II",
-      "Drug A", "Grade * Chemotherapy Treatment", "I * Drug A", "II * Drug A")
-  )
+  res <- mod %>% tidy_and_attach() %>% tidy_add_reference_rows()
   expect_equivalent(
     res$term,
-    c("(Intercept)", NA, "stage2", "stage3", "stage4", NA, "grade1",
-      "grade2", "trt1", NA, "grade1:trt1", "grade2:trt1")
+    c("(Intercept)", "stage_ref", "stage2", "stage3", "stage4", "grade1",
+      "grade2", "grade_ref", "trt1", "trt_ref", "grade1:trt1", "grade2:trt1")
   )
   expect_equivalent(
-    res$header_row,
-    c(FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE,
-      TRUE, FALSE, FALSE)
+    res$reference_row,
+    c(NA, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, TRUE,
+      NA, NA)
   )
 
-  # no effect of show_single_row when no reference row
-  res <- mod %>%
-    tidy_and_attach() %>%
-    tidy_identify_variables() %>%
-    tidy_add_header_rows(show_single_row = .$variable)
-  expect_equivalent(
-    res$label,
-    c("(Intercept)", "T Stage", "T2", "T3", "T4", "Grade", "I", "II",
-      "Drug A", "Grade * Chemotherapy Treatment", "I * Drug A", "II * Drug A")
+  # no reference row added if other contrasts are used
+  mod <- glm(
+    response ~ stage + grade * trt,
+    gtsummary::trial,
+    family = binomial,
+    contrasts = list(stage = contr.poly, grade = contr.helmert, trt = matrix(c(2, 3)))
   )
-  expect_equivalent(
-    res$term,
-    c("(Intercept)", NA, "stage2", "stage3", "stage4", NA, "grade1",
-      "grade2", "trt1", NA, "grade1:trt1", "grade2:trt1")
-  )
-  expect_equivalent(
-    res$header_row,
-    c(FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE,
-      TRUE, FALSE, FALSE)
-  )
-
-  # with reference rows
-  res <- mod %>%
-    tidy_and_attach() %>%
-    tidy_add_reference_rows() %>%
-    tidy_add_header_rows()
-  expect_equivalent(
-    res$label,
-    c("(Intercept)", "T Stage", "T1", "T2", "T3", "T4", "Grade",
-      "I", "II", "III", "Chemotherapy Treatment", "Drug A", "Drug B",
-      "Grade * Chemotherapy Treatment", "I * Drug A", "II * Drug A")
-  )
-  expect_equivalent(
-    res$term,
-    c("(Intercept)", NA, "stage_ref", "stage2", "stage3", "stage4",
-      NA, "grade1", "grade2", "grade_ref", NA, "trt1", "trt_ref", NA,
-      "grade1:trt1", "grade2:trt1")
-  )
-  expect_equivalent(
-    res$header_row,
-    c(FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE,
-      FALSE, TRUE, FALSE, FALSE, TRUE, FALSE, FALSE)
-  )
-
-  res <- mod %>%
-    tidy_and_attach() %>%
-    tidy_add_reference_rows() %>%
-    tidy_add_header_rows(show_single_row = c("trt", "unexist", "stage"))
-  expect_equivalent(
-    res$label,
-    c("(Intercept)", "T Stage", "T1", "T2", "T3", "T4", "Grade",
-      "I", "II", "III", "Chemotherapy Treatment", "Grade * Chemotherapy Treatment",
-      "I * Drug A", "II * Drug A")
-  )
-  expect_equivalent(
-    res$term,
-    c("(Intercept)", NA, "stage_ref", "stage2", "stage3", "stage4",
-      NA, "grade1", "grade2", "grade_ref", "trt1", NA, "grade1:trt1",
-      "grade2:trt1")
-  )
-  expect_equivalent(
-    res$header_row,
-    c(FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE,
-      FALSE, FALSE, TRUE, FALSE, FALSE)
-  )
-
-
+  res <- mod %>% tidy_and_attach() %>% tidy_add_reference_rows()
+  expect_true(all(is.na(res$reference_row)))
 })
 
 
-
-test_that("tidy_add_header_rows() works with nnet::multinom", {
-  mod <- nnet::multinom(grade ~ stage + marker + age + trt, data = gtsummary::trial, trace = FALSE)
-  res <- mod %>%
-    tidy_and_attach() %>%
-    tidy_add_reference_rows() %>%
-    tidy_add_header_rows()
-  expect_equivalent(
-    res$header_row,
-    c(FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE,
-      FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE,
-      FALSE, TRUE, FALSE, FALSE)
+test_that("tidy_add_reference_rows() works with different values of base in contr.treatment()", {
+  mod <- glm(
+    response ~ stage + grade * trt,
+    gtsummary::trial,
+    family = binomial,
+    contrasts = list(stage = contr.treatment(4, base = 3),
+                     grade = contr.treatment(3, base = 2),
+                     trt = contr.treatment(2, base = 2))
   )
-  res <- mod %>%
-    tidy_and_attach() %>%
-    tidy_add_reference_rows() %>%
-    tidy_add_header_rows(show_single_row = .$variable)
+  res <- mod %>% tidy_and_attach() %>% tidy_add_reference_rows()
   expect_equivalent(
-    res$header_row,
-    c(FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE,
-      FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)
+    res$term,
+    c("(Intercept)", "stage1", "stage2", "stage_ref", "stage4", "grade1",
+      "grade_ref", "grade3", "trt1", "trt_ref", "grade1:trt1", "grade3:trt1")
+  )
+  expect_equivalent(
+    res$reference_row,
+    c(NA, FALSE, FALSE, TRUE, FALSE, FALSE, TRUE, FALSE, FALSE, TRUE,
+      NA, NA)
+  )
+})
+
+test_that("tidy_add_reference_rows() works with nnet::multinom", {
+  mod <- nnet::multinom(grade ~ stage + marker + age, data = gtsummary::trial, trace = FALSE)
+  res <- mod %>% tidy_and_attach() %>% tidy_add_reference_rows()
+  expect_equivalent(
+    res$reference_row,
+    c(NA, TRUE, FALSE, FALSE, FALSE, NA, NA, NA, TRUE, FALSE, FALSE,
+      FALSE, NA, NA)
   )
 })
