7c1a40089cfb4ab6bc5f4ff21e0fddb5a514c97e
Joseph Larmarange <joseph@larmarange.net>
2020-09-02 09:49:33 +0200

    not needed (col order managed at the end)

--- R/tidy_identify_variables.R
+++ R/tidy_identify_variables.R
@@ -49,11 +49,6 @@ tidy_identify_variables <- function(x, model = tidy_get_model(x)) {
 
     x %>%
       dplyr::left_join(variables_list, by = "term") %>%
-      dplyr::select(
-        dplyr::any_of("y.level"), # for multinom models
-        .data$term, .data$variable, .data$var_class, .data$var_type,
-        dplyr::everything()
-      ) %>%
       dplyr::mutate(
         var_type = dplyr::if_else(
           is.na(.data$variable),
