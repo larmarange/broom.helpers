e28ed702e53f4be829a0ab2677dce7faaeae4106
Joseph Larmarange <joseph@larmarange.net>
2020-10-19 17:56:06 +0200

    first draft of model_get_n()

--- R/model_get_model_matrix.R
+++ R/model_get_model_matrix.R
@@ -6,6 +6,7 @@
 #' with the same structure as [stats::model.matrix.default()].
 #'
 #' @param model a model object
+#' @param ... additional arguments passed to [stats::model.matrix()]
 #' @export
 #' @family model_helpers
 #' @seealso [stats::model.matrix()]
@@ -13,18 +14,18 @@
 #' lm(hp ~ mpg + factor(cyl), mtcars) %>%
 #'   model_get_model_matrix() %>%
 #'   head()
-model_get_model_matrix <- function(model) {
+model_get_model_matrix <- function(model, ...) {
   UseMethod("model_get_model_matrix")
 }
 
 #' @export
 #' @rdname model_get_model_matrix
-model_get_model_matrix.default <- function(model) {
+model_get_model_matrix.default <- function(model, ...) {
   tryCatch(
-    stats::model.matrix(model),
+    stats::model.matrix(model, ...),
     error = function(e) {
       tryCatch( # test second approach
-        stats::model.matrix(stats::terms(model), model$model),
+        stats::model.matrix(stats::terms(model), model$model, ...),
         error = function(e) {
           NULL
         }
@@ -39,14 +40,14 @@ model_get_model_matrix.default <- function(model) {
 # consistent with the terms names when contrasts other
 # than treatment are used, resulting in an issue for
 # the identification of variables
-model_get_model_matrix.multinom <- function(model) {
-  mm <- stats::model.matrix(model)
+model_get_model_matrix.multinom <- function(model, ...) {
+  mm <- stats::model.matrix(model, ...)
   colnames(mm) <- colnames(stats::coef(model))
   mm
 }
 
 #' @export
 #' @rdname model_get_model_matrix
-model_get_model_matrix.clm <- function(model) {
-  stats::model.matrix(model)[[1]]
+model_get_model_matrix.clm <- function(model, ...) {
+  stats::model.matrix(model, ...)[[1]]
 }
---
+++ R/model_get_n.R
@@ -0,0 +1,127 @@
+#' Get the number of observations
+#'
+#' @param model a model object
+#' @export
+#' @family model_helpers
+#' @examples
+#' lm(hp ~ mpg + factor(cyl) + disp:hp, mtcars) %>%
+#'   model_get_n()
+#'
+#' mod <- glm(
+#'   response ~ stage * grade + trt,
+#'   gtsummary::trial,
+#'   family = binomial,
+#'   contrasts = list(stage = contr.sum, grade = contr.treatment(3, 2), trt = "contr.SAS")
+#' )
+#' mod %>% model_get_n()
+#'
+#' mod <- glm(Survived ~ Class * Age + Sex, data = Titanic %>% as.data.frame(), weights = Freq, family = binomial)
+#' mod %>% model_get_n()
+model_get_n <- function(model) {
+  UseMethod("model_get_n")
+}
+
+#' @export
+#' @rdname model_get_n
+model_get_n.default <- function(model) {
+  # add a warning if contr.poly or custom contrasts
+
+  # computation will be done using a term matrix (tm)
+  # adapted from model matrix
+  # only positive terms are taken into account
+  mf <- model %>% model_get_model_frame()
+  tryCatch({
+    mf2 <- mf %>%
+      dplyr::mutate(
+        dplyr::across(
+          c(where(is.numeric), -dplyr::any_of("(weights)")),
+          ~ 1L
+        )
+      )
+    tm <- model %>%
+      model_get_model_matrix(data = mf2)
+  },
+  error = function(e) {
+    tm <- NULL
+  })
+  if (is.null(tm)) {
+    tryCatch({
+      model2 <- model
+      model2$model <- model2$model %>%
+        dplyr::mutate(
+          dplyr::across(
+            c(where(is.numeric), -dplyr::any_of("(weights)")),
+            ~ 1L
+          )
+        )
+      tm <- model2 %>%
+        model_get_model_matrix()
+    },
+    error = function(e) {
+      tm <- NULL
+    })
+  }
+  if (is.null(tm)) {
+    return(NULL)
+  }
+
+  # getting weights
+  if ("(weights)" %in% colnames(mf)) {
+    weights <- mf[["(weights)"]]
+  } else {
+    weights <- rep.int(1L, nrow(mf))
+  }
+
+  # adding reference terms
+  # for treatment and sum contrasts
+  tl <- model %>%
+    model_list_terms_levels()
+  for (v in unique(tl$variable)) {
+    ct <- tl %>%
+      dplyr::filter(.data$variable == v) %>%
+      purrr::chuck("contrasts_type") %>%
+      dplyr::first()
+    ref_term <- tl %>%
+      dplyr::filter(.data$variable == v & reference) %>%
+      purrr::chuck("term")
+    nonref_terms <- tl %>%
+      dplyr::filter(.data$variable == v & !reference) %>%
+      purrr::chuck("term")
+
+    if (ct == "treatment" & all(nonref_terms %in% colnames(tm))) {
+      tm <- cbind(
+        tm,
+        matrix(
+          as.integer(
+            rowSums(tm[, nonref_terms, drop = FALSE] == 0L) ==
+              length(nonref_terms)
+          ),
+          ncol = 1,
+          dimnames = list(NULL, ref_term)
+        )
+      )
+    }
+    if (ct == "sum" & all(nonref_terms %in% colnames(tm))) {
+      tm <- cbind(
+        tm,
+        matrix(
+          as.integer(
+            rowSums(tm[, nonref_terms, drop = FALSE] == -1L) ==
+              length(nonref_terms)
+          ),
+          ncol = 1,
+          dimnames = list(NULL, ref_term)
+        )
+      )
+    }
+  }
+
+  res <- dplyr::tibble(
+    term = colnames(tm),
+    n = colSums((tm > 0) * weights),
+    n_unweighted = colSums(tm > 0)
+  )
+  attr(res, "N") <- sum(weights)
+  attr(res, "N_unweighted") <- nrow(mf)
+  res
+}
