e81b7267e45a1653aa8ecc368e29ea6528991453
Joseph Larmarange <joseph@larmarange.net>
2020-12-14 11:19:14 +0100

    move emmeans from Imports to Suggests

--- R/tidy_add_estimate_to_reference_rows.R
+++ R/tidy_add_estimate_to_reference_rows.R
@@ -8,7 +8,8 @@
 #' For categorical variables with a sum contrast ([stats::contr.sum()]),
 #' the estimate value of the reference row will be equal to the sum of
 #' all other coefficients multiplied by `-1` (eventually exponentiated if
-#' `exponentiate = TRUE`), and obtained with [stats::dummy.coef()].
+#' `exponentiate = TRUE`), and obtained with [emmeans::emmeans()].
+#' The `emmeans` package should therefore be installed.
 #' For sum contrasts, the model coefficient corresponds
 #' to the difference of each level with the grand mean.
 #'
@@ -115,6 +116,13 @@ tidy_add_estimate_to_reference_rows <- function(
         "Reference row of variable '", variable, "' remained unchanged."
       ))
   } else {
+    if (!requireNamespace("emmeans")) {
+      stop( # nocov start
+        "'emmeans' package is required to add an estimate to reference rows ",
+        "of categorical variables coded with sum contrasts. ",
+        "Please install 'emmeans'."
+      ) # nocov end
+    }
     dc <- tryCatch(
       suppressMessages(
         emmeans::emmeans(model, specs = variable, contr = "eff")
