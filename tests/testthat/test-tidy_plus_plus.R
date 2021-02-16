b05a7f8feb00c3bf818704096e0135d978c800c4
Joseph Larmarange <joseph@larmarange.net>
2021-02-16 15:37:33 +0100

    skip of brms too old

--- tests/testthat/test-tidy_plus_plus.R
+++ tests/testthat/test-tidy_plus_plus.R
@@ -344,6 +344,8 @@ test_that("tidy_plus_plus() works with gam::gam", {
 test_that("tidy_plus_plus() works with brms::brm", {
   skip_if_not_installed("broom.mixed")
   skip_if_not_installed("brms")
+  skip_if(packageVersion("brms") < 2.13)
+
   load(system.file("extdata", "brms_example.rda", package="broom.mixed"))
   mod <- brms_crossedRE
   expect_error(
