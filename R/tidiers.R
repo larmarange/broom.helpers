f5d1fa0116f19f7d7dfa124e968550ab4f02cdcc
Joseph Larmarange <joseph@larmarange.net>
2021-03-09 16:34:17 +0100

    file organisation and bug fix

---
+++ R/tidiers.R
@@ -0,0 +1,27 @@
+#' Tidy a model with parameters package
+#'
+#' Use `parameters::model_parameters()` to tidy a model and apply
+#' `parameters::standardize_names(style = "broom")` to the output
+#' @param x a model
+#' @param conf.int logical indicating whether or not to include a confidence
+#' interval in the tidied output
+#' @param conf.level the confidence level to use for the confidence interval
+#' @param ... additional parameters passed to `parameters::model_parameters()`
+#' @examples
+#' lm(Sepal.Length ~ Sepal.Width + Species, data = iris) %>%
+#'   tidy_parameters()
+#' \dontrun{
+#'   lm(Sepal.Length ~ Sepal.Width + Species, data = iris) %>%
+#'     tidy_parameters(bootstrap = TRUE)
+#' }
+#' @export
+tidy_parameters <- function(x, conf.int = TRUE, conf.level = .95, ...) {
+  if (!requireNamespace("parameters"))
+    stop("You need to install 'parameters' to use 'tidy_parameters'.")
+
+  if (!conf.int) conf.level <- NULL
+
+  x %>%
+    parameters::model_parameters(ci = conf.level, ...) %>%
+    parameters::standardize_names(style = "broom")
+}
