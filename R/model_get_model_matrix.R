6d4a23897512af9e91fa1be1d9d33aad38749da1
Joseph <joseph@larmarange.net>
2021-08-06 15:06:27 +0200

    methods for glmmTMB to take into account all components

--- R/model_get_model_matrix.R
+++ R/model_get_model_matrix.R
@@ -57,3 +57,23 @@ model_get_model_matrix.clm <- function(model, ...) {
 model_get_model_matrix.brmsfit <- function(model, ...) {
   model %>% brms::standata() %>% purrr::pluck("X")
 }
+
+#' @export
+#' @rdname model_get_model_matrix
+#' @details
+#' For models fitted with [glmmTMB::glmmTMB()], it will return a model matrix
+#' taking into account all components ("cond", "zi" and "disp"). For a more
+#' restricted model matrix, please refer to [glmmTMB::model.matrix.glmmTMB()].
+model_get_model_matrix.glmmTMB <- function(model, ...) {
+  # load lme4 if available
+  if (!requireNamespace("lme4", quietly = TRUE))
+      stop("'lme4' package is required for glmmTMB model.") # nocov
+
+  stats::model.matrix(
+    lme4::nobars(model$modelInfo$allForm$combForm),
+    stats::model.frame(model, ..),
+    contrasts.arg = model$modelInfo$contrasts
+  )
+}
+
+
