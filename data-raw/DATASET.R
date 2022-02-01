4861376e2586d7bea9dbac01f65d07203e2e1cb5
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-02-01 13:08:39 -0500

    exporting tibble of supported models

---
+++ data-raw/DATASET.R
@@ -0,0 +1,37 @@
+supported_models <-
+  tibble::tribble(
+    ~model, ~notes,
+    '`stats::lm()`', '',
+    '`stats::glm()`', '',
+    '`stats::aov()`', 'Reference rows are not relevant for such models.',
+    '`ordinal::clm()`', 'Limited support for models with nominal predictors.',
+    '`ordinal::clmm()`', 'Limited support for models with nominal predictors.',
+    '`survival::coxph()`', '',
+    '`survival::survreg()`', '',
+    '`survival::clogit()`', '',
+    '`lme4::lmer()`', '`broom.mixed` package required',
+    '`lme4::glmer()`', '`broom.mixed` package required',
+    '`lme4::glmer.nb()`', '`broom.mixed` package required',
+    '`brms::brm()`', '`broom.mixed` package required',
+    '`geepack::geeglm()`', '',
+    '`gam::gam()`', '',
+    '`glmmTMB::glmmTMB()`', '`broom.mixed` package required',
+    '`mgcv::gam()`', 'Use default tidier `broom::tidy()` for smooth terms only, or `gtsummary::tidy_gam()` to include parametric terms',
+    '`nnet::multinom()`', '',
+    '`survey::svyglm()`', '',
+    '`survey::svycoxph()`', '',
+    '`survey::svyolr()`', '',
+    '`MASS::polr()`', '',
+    '`MASS::glm.nb()`', '',
+    '`mice::mira`', 'Limited support. If `mod` is a `mira` object, use `tidy_plus_plus(mod, tidy_fun = function(x, ...) mice::pool(x) %>% mice::tidy(...))`',
+    '`lavaan::lavaan()`', 'Limited support for categorical variables',
+    '`stats::nls()`', 'Limited support',
+    '`lfe::felm()`', '',
+    '`rstanarm::stan_glm()`', '`broom.mixed` package required',
+    '`VGAM::vglm()`', 'Limited support. It is recommended to use `tidy_parameters()` as `tidy_fun`.',
+    '`cmprsk::crr()`', 'Limited support. It is recommended to use `tidycmprsk::crr()` instead.',
+    '`tidycmprsk::crr()`', '',
+    '`plm::plm()`',''
+  )
+
+usethis::use_data(supported_models, overwrite = TRUE)
