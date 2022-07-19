ccdad767ba5a00b9e38f072a2511d838aebeb36a
Joseph Larmarange <joseph@larmarange.net>
2022-07-19 20:45:11 +0200

    test update

--- tests/testthat/test-assert_package.R
+++ tests/testthat/test-assert_package.R
@@ -6,23 +6,26 @@ test_that(".assert_package() works", {
   )
   expect_true(.assert_package("broom", boolean = TRUE))
 
+  expect_false(.assert_package("br000000m", boolean = TRUE))
+
+  mv <- c(Suggests  = "1.1.28")
+  attr(mv, "compare") <- ">="
+  expect_equal(
+    .get_min_version_required("lme4"),
+    mv
+  )
+  expect_equal(
+    .get_min_version_required("brms", pkg_search = NULL),
+    NULL
+  )
+
+  skip_if(interactive())
   # expect an error msg for pkg that doesn't exist
+  # note: if interactive(), user will be invited to install the missing pkg
   expect_error(
     .assert_package("br000000m")
   )
   expect_error(
     .assert_package("br000000m", fn = "test_fun()")
-  )
-  expect_false(.assert_package("br000000m", boolean = TRUE))
-
-  # for some reason `packageDescription()` doesn't grab the package info in GH Actions
-  skip_if(is.null(.get_min_version_required("lme4")))
-  expect_equal(
-    .get_min_version_required("lme4"),
-    c(Suggests  = "1.1.28")
-  )
-  expect_equal(
-    .get_min_version_required("brms", pkg_search = NULL),
-    NULL
   )
 })
