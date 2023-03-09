1dd90286830e1a6521553acee62d2a35c80025ae
Joseph Larmarange <joseph@larmarange.net>
2023-03-09 16:40:59 +0100

    force tibble

--- R/tidy_add_estimate_to_reference_rows.R
+++ R/tidy_add_estimate_to_reference_rows.R
@@ -165,12 +165,13 @@ tidy_add_estimate_to_reference_rows <- function(
     )
   } else {
     res <- dc$contrasts %>%
-      as.data.frame() %>%
+      dplyr::as_tibble() %>%
       dplyr::last() %>%
       dplyr::select("estimate", std.error = "SE", "p.value")
     ci <- dc %>%
       emmeans::contrast() %>%
       confint() %>%
+      dplyr::as_tibble() %>%
       dplyr::last()
     res$conf.low <- ci$asymp.LCL
     res$conf.high <- ci$asymp.UCL
