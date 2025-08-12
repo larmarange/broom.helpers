619de11511d0e96502bd53dab8d7c2464a4877d4
Joseph Larmarange <joseph@larmarange.net>
2025-08-12 11:58:14 +0200

    lint

--- tests/testthat/test-tidy_plus_plus.R
+++ tests/testthat/test-tidy_plus_plus.R
@@ -726,7 +726,7 @@ test_that("tidy_plus_plus() works with fixest models", {
   )
   expect_equal(nrow(res[res$instrumental, ]), 3L)
 
-  mod <- fixest::feols(mpg ~ cyl, data = mtcars[mtcars$carb != 1,])
+  mod <- fixest::feols(mpg ~ cyl, data = mtcars[mtcars$carb != 1, ])
   expect_no_error(
     res <- mod |> tidy_plus_plus()
   )
